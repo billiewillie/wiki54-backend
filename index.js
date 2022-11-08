b7f488f38d54cba25e7294dc3ca5305d35eec210
Vadim Belinovich <vadimbelinovich@gmail.com>
2022-11-08 16:55:55 +0300

    added search

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ import fs from "fs";
 import multer from "multer";
 import cors from "cors";
 import mongoose from "mongoose";
+import { Users } from "./users.js";
 
-import { checkAuth, handleValidationErrors } from "./utils/index.js";
-import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
-import { UserController, PostController } from "./controllers/index.js";
+// import { checkAuth, handleValidationErrors } from "./utils/index.js";
+// import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
+// import { UserController, PostController } from "./controllers/index.js";
 
-mongoose
-	.connect("mongodb+srv://admin:Blin-1987@cluster0.bem0i2q.mongodb.net/blog?retryWrites=true&w=majority")
-	.then(() => console.log("DB ok"))
-	.catch((err) => console.log("DB error", err));
+// mongoose
+// 	.connect("mongodb+srv://admin:Blin-1987@cluster0.bem0i2q.mongodb.net/blog?retryWrites=true&w=majority")
+// 	.then(() => console.log("DB ok"))
+// 	.catch((err) => console.log("DB error", err));
 
 const app = express();
 
@@ -33,9 +34,9 @@ app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
-app.get("/auth/me", checkAuth, UserController.getMe);
-app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
-app.post("/auth/register", registerValidation, handleValidationErrors, UserController.register);
+// app.get("/auth/me", checkAuth, UserController.getMe);
+// app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
+// app.post("/auth/register", registerValidation, handleValidationErrors, UserController.register);
 
 app.post("/upload", upload.single("image"), (req, res) => {
 	res.json({
@@ -43,11 +44,24 @@ app.post("/upload", upload.single("image"), (req, res) => {
 	});
 });
 
-app.get("/posts", PostController.getAll);
-app.get("/posts/:id", PostController.getOne);
-app.delete("/posts/:id", checkAuth, PostController.remove);
-app.post("/posts", checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
-app.patch("/posts/:id", checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
+app.get("/", (req, res) => {
+	const { q } = req.query;
+	const keys = ["name", "surname", "email"];
+
+	console.log(req.query);
+
+	const search = (data) => {
+		return data.filter((item) => keys.some((key) => item[key].toLowerCase().includes(q)));
+	};
+
+	q ? res.json(search(Users).slice(0, 5)) : res.json(Users.slice(0, 5));
+});
+
+// app.get("/posts", PostController.getAll);
+// app.get("/posts/:id", PostController.getOne);
+// app.delete("/posts/:id", checkAuth, PostController.remove);
+// app.post("/posts", checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+// app.patch("/posts/:id", checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
 app.listen(4444, (err) => {
 	if (err) return console.log(err);
---
+++ users.js
@@ -0,0 +1,37 @@
+export const Users = [
+	{
+		id: 1,
+		name: "John",
+		surname: "mongoose",
+		email: "heefes@dsfs.com",
+		gender: "male",
+	},
+	{
+		id: 2,
+		name: "Dima",
+		surname: "wowowood",
+		email: "dmitri@tttt.com",
+		gender: "male",
+	},
+	{
+		id: 3,
+		name: "Michael",
+		surname: "Nights",
+		email: "nights@michael.com",
+		gender: "male",
+	},
+	{
+		id: 4,
+		name: "Frank",
+		surname: "PostController",
+		email: "frank@ostController.ru",
+		gender: "male",
+	},
+	{
+		id: 5,
+		name: "Liza",
+		surname: "Types",
+		email: "liza@types.com",
+		gender: "female",
+	},
+];
