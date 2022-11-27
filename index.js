0cf513365f1816220bb9b6241d12a68db85499d6
Vadim <vadimbelinovich@gmail.com>
2022-11-27 03:38:12 +0300

    edited localhost

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { UserController, PostController } from './controllers/index.js';
 import { loginValidation, postCreateValidation } from './validations.js';
 
 mongoose
-	.connect('mongodb://localhost:27017/wiki54')
+	.connect('mongodb://127.0.0.1:27017/wiki54')
 	.then(() => console.log('DB ok'))
 	.catch((err) => console.log('DB error', err));
 
@@ -36,57 +36,57 @@ app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
-// app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
+// // app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
 
-app.post('/upload', upload.single('image'), (req, res) => {
-	res.json({
-		url: `/uploads/${req.file.originalname}`,
-	});
-});
+// app.post('/upload', upload.single('image'), (req, res) => {
+// 	res.json({
+// 		url: `/uploads/${req.file.originalname}`,
+// 	});
+// });
 
-app.get('/', (req, res) => {
-	const { q } = req.query;
-	const keys = ['name', 'surname', 'email'];
+// app.get('/', (req, res) => {
+// 	const { q } = req.query;
+// 	const keys = ['name', 'surname', 'email'];
 
-	const search = (data) => {
-		return data.filter((item) => keys.some((key) => item[key].toLowerCase().includes(q)));
-	};
+// 	const search = (data) => {
+// 		return data.filter((item) => keys.some((key) => item[key].toLowerCase().includes(q)));
+// 	};
 
-	q ? res.json(search(Users).slice(0, 5)) : res.json(Users.slice(0, 5));
-});
+// 	q ? res.json(search(Users).slice(0, 5)) : res.json(Users.slice(0, 5));
+// });
 
-app.get('/:department', (req, res) => {
-	const { department } = req.params;
-	const search = (data) => {
-		return data.filter((item) => item.department === department);
-	};
-	res.json(search(Posts));
-});
+// app.get('/:department', (req, res) => {
+// 	const { department } = req.params;
+// 	const search = (data) => {
+// 		return data.filter((item) => item.department === department);
+// 	};
+// 	res.json(search(Posts));
+// });
 
-app.get('/:department/:id', (req, res) => {
-	const { id } = req.params;
-	const search = (data) => {
-		return data.filter((item) => item.id === Number(id))[0];
-	};
-	res.json(search(Posts));
-});
+// app.get('/:department/:id', (req, res) => {
+// 	const { id } = req.params;
+// 	const search = (data) => {
+// 		return data.filter((item) => item.id === Number(id))[0];
+// 	};
+// 	res.json(search(Posts));
+// });
 
-app.get('/:department/:id/edit', (req, res) => {
-	const { id } = req.params;
-	const search = (data) => {
-		return data.filter((item) => item.id === Number(id))[0];
-	};
-	res.json(search(Posts));
-});
+// app.get('/:department/:id/edit', (req, res) => {
+// 	const { id } = req.params;
+// 	const search = (data) => {
+// 		return data.filter((item) => item.id === Number(id))[0];
+// 	};
+// 	res.json(search(Posts));
+// });
 
-app.post('/:department/:id', async (req, res) => {
-	const { id } = req.params;
-	console.log(req.body);
-});
+// app.post('/:department/:id', async (req, res) => {
+// 	const { id } = req.params;
+// 	console.log(req.body);
+// });
 
-app.post('/:department/createPost', async (req, res) => {
-	console.log(req.body);
-});
+// app.post('/:department/createPost', async (req, res) => {
+// 	console.log(req.body);
+// });
 
 // app.get("/posts", PostController.getAll);
 // app.get("/posts/:id", PostController.getOne);
