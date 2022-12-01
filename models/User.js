c95739271a95794f14d8095403f81cf2ccac5791
Vadim Belinovich <vadimbelinovich@gmail.com>
2022-12-01 17:09:53 +0300

    edited

--- models/User.js
+++ models/User.js
@@ -12,11 +12,17 @@ const UserSchema = new mongoose.Schema({
 	},
 	department: {
 		type: String,
+		default: 'studia54',
 		required: [true, 'напишите ваш отдел пожалуйста'],
 	},
 	role: {
 		type: String,
+		default: 'user',
 		required: [true, 'укажите роль пользователя на сайте пожалуйста'],
+	},
+	isMapOpened: {
+		type: Boolean,
+		default: false,
 	},
 });
 
