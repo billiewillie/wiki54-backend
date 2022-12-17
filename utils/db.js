9770d98b72490b3ae94d3ee3551d172f0195ba59
Vadim Belinovich <vadimbelinovich@gmail.com>
2022-12-17 18:22:18 +0300

    added

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
+		await mongoose.connect(process.env.MONGO_URI_CLOUD);
 		console.log(`DB Ok`);
 	} catch (error) {
 		console.log(error);
